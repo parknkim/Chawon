a40eb42e7d8b0b72c3ba41b92242c0a1cd2b5acf
parknkim <parknkim@gmail.com>
2017-07-05 13:39:49 +0900

    Add files via upload
    update refering to the textbook named "Machine Learning, Deep learning by using Python"

--- bs-usd.py
+++ bs-usd.py
@@ -10,19 +10,18 @@ soup = BeautifulSoup(res, "html.parser")
 #print("usd/krw", price)
 
 #refer to the page 37 on 파이선을 이용한 머신러닝, 딥러닝, 실전개발 입문
-price = soup.select("div.head_info > span.value")[0].string
-print("usd/krw --> ", price)
-price = soup.select("div.head_info > span.value")[1].string
-print("100*JYP/krw --> ", price)
+for i in range(12):
+    currency = soup.select("h3.h_lst > span.blind")[i].string
+    price = soup.select("div.head_info > span.value")[i].string
+    time = soup.select("div.graph_info > span.time")[i].string
+    print(currency,":", price, " in", time)
 
 '''
 list_price = soup.select("div.head_info > span.value")
 list_nation = soup.select("h3.h_lst > span.blind")
-
 for a, in list_price:
     name = a.string
     print(name)
-
 for b in list_nation:
     name = b.string
     print(name)

54949012d3dc5e4e451f27c8051e9dd333e3c478
parknkim <parknkim@gmail.com>
2017-07-05 13:30:31 +0900

    Add files via upload

--- bs-usd.py
+++ bs-usd.py
@@ -9,7 +9,21 @@ soup = BeautifulSoup(res, "html.parser")
 #price = soup.select_one("div.head_info > span.value").string
 #print("usd/krw", price)
 
+#refer to the page 37 on 파이선을 이용한 머신러닝, 딥러닝, 실전개발 입문
+price = soup.select("div.head_info > span.value")[0].string
+print("usd/krw --> ", price)
+price = soup.select("div.head_info > span.value")[1].string
+print("100*JYP/krw --> ", price)
+
+'''
 list_price = soup.select("div.head_info > span.value")
-for a in list_price:
+list_nation = soup.select("h3.h_lst > span.blind")
+
+for a, in list_price:
     name = a.string
     print(name)
+
+for b in list_nation:
+    name = b.string
+    print(name)
+'''

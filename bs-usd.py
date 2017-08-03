990096c5a74034ab18650dcde793662828f170ee
parknkim <parknkim@gmail.com>
2017-08-03 14:28:55 +0900

    Add files via upload

--- bs-usd.py
+++ bs-usd.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
 
-url="http://info.finance.naver.com/marketindex/"
+url = "http://info.finance.naver.com/marketindex/"
+url01 = "http://finance.naver.com/sise/"
+
 res = req.urlopen(url)
+res01 = req.urlopen(url01)
 
 soup = BeautifulSoup(res, "html.parser")
+soup01 = BeautifulSoup(res01, "html.parser")
 
 #price = soup.select_one("div.head_info > span.value").string
 #print("usd/krw", price)
@@ -12,7 +16,7 @@ soup = BeautifulSoup(res, "html.parser")
 time = soup.select("div.graph_info > span.time")[9].string
 filename = "Currency"+time+".txt"
 #print(filename)
-file = open(filename,'w')
+file = open(filename,'a')
 
 #refer to the page 37 on 파이선을 이용한 머신러닝, 딥러닝, 실전개발 입문
 for i in range(12):
@@ -24,6 +28,12 @@ for i in range(12):
     file.write(str(currency+': \t'+price+' \t in '+time)+'\n')
 file.close()
 print("File name: ",filename)
+
+
+#kospi = soup01.select_one("div.lft > span.blind").string
+#index = soup01.select("span.num num3").string
+#line01 = str(kospi+': \t'+index)
+#print(line01)
 
 '''
 list_price = soup.select("div.head_info > span.value")

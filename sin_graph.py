d57e4f9502464a940507a81cbcf0c78e94eac6d7
parknkim <parknkim@gmail.com>
2017-08-08 09:26:26 +0900

    Add files via upload

--- sin_graph.py
+++ sin_graph.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 6, 0.1)
+step = 0.1
+low = -2*np.pi
+high = 2*np.pi
+
+#x = np.arange(0, 6, 0.1)
+x = np.arange(low, high, step)
 y = np.sin(x)
 
 plt.plot(x, y)
